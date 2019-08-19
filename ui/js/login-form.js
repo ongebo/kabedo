19df305eb86ebdf157b8adceda14cbda08a19e81
ongebo <isaacongebo@gmail.com>
2019-08-19 19:47:02 +0300

    Design ui/ux for profile update
    - Add HTML, CSS, and JS for profile update ui/ux
    - Change JS indentation from two to four spaces
    - Comment HTML sections in homepage.html
    - Limit input styles in styles.css to form elements
    
    [Finishes #164847546]

--- ui/js/login-form.js
+++ ui/js/login-form.js
@@ -5,20 +5,20 @@ var loginModalContent = document.querySelector(".login-modal-content");
 var body = document.querySelector("body");
 
 function toggleModal() {
-  // Display or hide the Login modal.
-  loginModal.classList.toggle("show-modal");
+    // Display or hide the Login modal.
+    loginModal.classList.toggle("show-modal");
 
-  // Apply animation to the login form.
-  loginModalContent.classList.toggle("show-login-modal-content");
+    // Apply animation to the login form.
+    loginModalContent.classList.toggle("show-login-modal-content");
 
-  // Toggle scrollability of the modal background.
-  body.classList.toggle("fixed");
+    // Toggle scrollability of the modal background.
+    body.classList.toggle("fixed");
 }
 
 loginButton.addEventListener("click", toggleModal);
 closeButton.addEventListener("click", toggleModal);
 window.addEventListener("click", function(e) {
-  if (e.target === loginModal) {
-    toggleModal();
-  }
+    if (e.target === loginModal) {
+        toggleModal();
+    }
 });
--- ui/js/password-reset.js
+++ ui/js/password-reset.js
@@ -2,17 +2,17 @@ var passwordResetButton = document.getElementById("reset-password");
 passwordResetButton.addEventListener("click", notifyUser);
 
 function notifyUser() {
-  var message = document.querySelector(".password-reset-message");
-  var dialog = document.querySelector(".password-reset-dialog");
-  toggleClasses(message, dialog);
-  passwordResetButton.disabled = true;
-  setTimeout(() => {
+    var message = document.querySelector(".password-reset-message");
+    var dialog = document.querySelector(".password-reset-dialog");
     toggleClasses(message, dialog);
-    passwordResetButton.disabled = false;
-  }, 3500);
+    passwordResetButton.disabled = true;
+    setTimeout(() => {
+        toggleClasses(message, dialog);
+        passwordResetButton.disabled = false;
+    }, 3500);
 }
 
 function toggleClasses(message, dialog) {
-  message.classList.toggle("show-reset-message");
-  dialog.classList.toggle("show-reset-dialog");
+    message.classList.toggle("show-reset-message");
+    dialog.classList.toggle("show-reset-dialog");
 }
